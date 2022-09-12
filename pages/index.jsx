024220ae33ff33b925e8bfa907c480205e78e64c
tmacmax01 <565362767@qq.com>
2022-09-12 21:42:52 +0800

    week4
    week4

--- pages/index.jsx
+++ pages/index.jsx
@@ -11,7 +11,7 @@ const Home = () => {
   const fetchNFTs = async() => {
     let nfts;
     console.log("fetching nfts");
-    const api_key = "bR6bSJGU3qm0tfx67XMe-pf9OAqQh9hJ"
+    const api_key = "9Fe3ZoJ4BpLTLiVZ_l7myojKYQF0upqh"
     const baseURL = `https://eth-mainnet.alchemyapi.io/v2/${api_key}/getNFTs/`;
     var requestOptions = {
       method: 'GET'
@@ -39,7 +39,7 @@ const Home = () => {
       var requestOptions = {
         method: 'GET'
       };
-      const api_key = "bR6bSJGU3qm0tfx67XMe-pf9OAqQh9hJ"
+      const api_key = "9Fe3ZoJ4BpLTLiVZ_l7myojKYQF0upqh"
       const baseURL = `https://eth-mainnet.alchemyapi.io/v2/${api_key}/getNFTsForCollection/`;
       const fetchURL = `${baseURL}?contractAddress=${collection}&withMetadata=${"true"}`;
       const nfts = await fetch(fetchURL, requestOptions).then(data => data.json())
